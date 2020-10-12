654382e00aa076dea1a30121424fb9aaa6db7733
Ефимов Александр <a.efimov@inenergy.ru>
2020-10-12 18:39:09 +0300

    initial

---
+++ android/app/src/main/kotlin/com/example/hellow_ork/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hellow_ork
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,154 @@
+/// Flutter code sample for BottomNavigationBar
+
+// This example shows a [BottomNavigationBar] as it is used within a [Scaffold]
+// widget. The [BottomNavigationBar] has three [BottomNavigationBarItem]
+// widgets and the [currentIndex] is set to index 0. The selected item is
+// amber. The `_onItemTapped` function changes the selected item's index
+// and displays a corresponding message in the center of the [Scaffold].
+//
+// ![A scaffold with a bottom navigation bar containing three bottom navigation
+// bar items. The first one is selected.](https://flutter.github.io/assets-for-api-docs/assets/material/bottom_navigation_bar.png)
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+/// This is the main application widget.
+class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+/// This is the stateful widget that the main application instantiates.
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Controls',
+      style: optionStyle,
+    ),
+    NestedTabBar(),
+    Text(
+      'Index 1: Charts',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.gamepad),
+            label: 'Controls',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.battery_charging_full),
+            label: 'Power sources',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.show_chart),
+            label: 'Charts',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class NestedTabBar extends StatefulWidget {
+  @override
+  _NestedTabBarState createState() => _NestedTabBarState();
+}
+
+class _NestedTabBarState extends State<NestedTabBar>
+    with TickerProviderStateMixin {
+  TabController _nestedTabController;
+  @override
+  void initState() {
+    super.initState();
+    _nestedTabController = new TabController(length: 2, vsync: this);
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    _nestedTabController.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        TabBar(
+          controller: _nestedTabController,
+          indicatorColor: Colors.orange,
+          labelColor: Colors.orange,
+          unselectedLabelColor: Colors.black54,
+          isScrollable: true,
+          tabs: <Widget>[
+            Tab(
+              text: "Battery",
+            ),
+            Tab(
+              text: "FuelCell",
+            ),
+
+          ],
+        ),
+        Container(
+          height: screenHeight * 0.70,
+          margin: EdgeInsets.only(left: 16.0, right: 16.0),
+          child: TabBarView(
+            controller: _nestedTabController,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  color: Colors.blueGrey[300],
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  color: Colors.blueGrey[300],
+                ),
+              ),
+
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
